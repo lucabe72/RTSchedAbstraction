5b7c6c4ee222cc75ac7a9bb11c60a9bb9799fd6c
luca abeni <luca.abeni@santannapisa.it>
2025-11-06 09:27:09 +0100

    More realistic lists implementation
    All the functions are taken from include/linux/list.h in the Linux
    kernel.
    
    TODO: maybe extract the functions automatically

--- stub/list.h
+++ stub/list.h
@@ -8,24 +8,54 @@ static inline void INIT_LIST_HEAD(struct list_head *list)
 	list->prev = list;
 }
 
-
+static inline void __list_del(struct list_head * prev, struct list_head * next)
+{
+	next->prev = prev;
+	prev->next = next;
+}
+static inline void __list_del_entry(struct list_head *entry)
+{
+	__list_del(entry->prev, entry->next);
+}
 static inline void list_del_init(struct list_head *entry)
 {
+	__list_del_entry(entry);
+	INIT_LIST_HEAD(entry);
 }
+
 static inline int list_empty(const struct list_head *head)
 {
-  return 0;
+  return head->next == head;
+}
+
+static inline void __list_add(struct list_head *new,
+			      struct list_head *prev,
+			      struct list_head *next)
+{
+  next->prev = new;
+  new->next = next;
+  new->prev = prev;
+  prev->next = new;
 }
 static inline void list_add(struct list_head *new, struct list_head *head)
 {
+  __list_add(new, head, head->next);
 }
+
 static inline void list_add_tail(struct list_head *new, struct list_head *head)
 {
+  __list_add(new, head->prev, head);
 }
+
 static inline void list_move(struct list_head *list, struct list_head *head)
 {
+  __list_del_entry(list);
+  list_add(list, head);
 }
 static inline void list_move_tail(struct list_head *list, struct list_head *head)
 {
+  __list_del_entry(list);
+  list_add_tail(list, head);
 }
-#define list_entry(ptr, type, member) NULL
+
+#define list_entry(ptr, type, member) container_of(ptr, type, member)
